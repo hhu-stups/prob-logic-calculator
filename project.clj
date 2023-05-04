6d174f537539c07bc04a0752761ca4afc36db733
dgelessus <dgelessus@users.noreply.github.com>
2023-05-04 10:53:21 +0200

    Update ProB Java API to version 4.12.1

--- project.clj
+++ project.clj
@@ -5,17 +5,14 @@
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :plugins [[lein-ring "0.12.5"]]
   :java-source-paths ["src/java"]
-  :repositories [["snaps" "https://oss.sonatype.org/content/repositories/snapshots/"]]
+  ;;:repositories [["snaps" "https://oss.sonatype.org/content/repositories/snapshots/"]]
   :dependencies [
                  [ch.qos.logback/logback-classic "1.3.7"]
                  [org.clojure/clojure "1.8.0"]
                  [compojure "1.6.1"]
                  [org.clojure/data.json "0.2.6"]
                  [org.clojure/core.async "0.4.490"]
-; TODO: remove tla2BAST SNAPSHOT dependency once prob2.kernel
-;; contains a released version 1.1.5 of tla2BAST
-                 [de.hhu.stups/tla2bAST "1.1.5-SNAPSHOT"]
-                 [de.hhu.stups/de.prob2.kernel "3.15.0"]
+                 [de.hhu.stups/de.prob2.kernel "4.12.1"]
                  [hiccup "1.0.5"]
                  [ring/ring-core "1.7.1"]
                  ]
